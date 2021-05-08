f4899ff1a446507b1efeab93b06a0c30e6d6b10b
Christopher Ryan Limoa <clim0061@student.monash.edu>
2021-05-09 00:47:08 +1000

    Modify linechart to accept dynamic data

--- src/components/linechart.tsx
+++ src/components/linechart.tsx
@@ -1,18 +1,20 @@
+import { Children } from "react";
 import { Line } from "react-chartjs-2";
 
-const wptas_data = {
-    labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
-    datasets: [
-        {
-            label: "WPTAS",
-            data: [5, 6, 9, 9, 10, 12, 12],
-            fill: true,
-            backgroundColor: "rgba(75,192,192,0.2)",
-            borderColor: "rgba(75,192,192,1)",
-            tension: 0.3,
-        },
-    ],
-};
+// // static data example
+// const wptas_data = {
+//     labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
+//     datasets: [
+//         {
+//             label: "WPTAS",
+//             data: [5, 6, 9, 9, 10, 12, 12],
+//             fill: true,
+//             backgroundColor: "rgba(75,192,192,0.2)",
+//             borderColor: "rgba(75,192,192,1)",
+//             tension: 0.3,
+//         },
+//     ],
+// };
 
 const options = {
     scales: {
@@ -23,11 +25,38 @@ const options = {
     },
 };
 
-export function Linechart() {
-    return (
-        <div>
-            <h1>Line Chart</h1>
-            <Line data={wptas_data} options={options} type="line" />
-        </div>
-    );
+export type LinechartProps = {
+    data: Array<number>;
+    test: string;
+};
+
+export function Linechart({ data, test }: LinechartProps) {
+    // dynamic data example
+    let label, backgroundColor, borderColor;
+
+    if (test == "wptas") {
+        label = "WPTAS";
+        backgroundColor = "rgba(75,192,192,0.2)";
+        borderColor = "rgba(75,192,192,1)";
+    } else if (test == "abs") {
+        label = "ABS";
+        backgroundColor = "rgba(192,104,75,0.2)";
+        borderColor = "rgba(192,104,75,1)";
+    }
+
+    const test_data = {
+        labels: [...Array(data.length).keys()].map((x) => "Day " + (x + 1)),
+        datasets: [
+            {
+                label: label,
+                data: data,
+                fill: true,
+                backgroundColor: backgroundColor,
+                borderColor: borderColor,
+                tension: 0.2,
+            },
+        ],
+    };
+
+    return <Line data={test_data} options={options} type="line" />;
 }
--- src/routes/analysis.tsx
+++ src/routes/analysis.tsx
@@ -1,7 +1,13 @@
 import { Linechart } from "@/components";
 
+const wptas_data = Array(5, 6, 9, 9, 10, 4, 6, 10, 12);
+const abs_data = Array(52, 24, 26, 42, 33, 27, 49, 50, 54);
+
 export function Analysis() {
     return (
-        <Linechart></Linechart>
+        <div>
+            <Linechart data={wptas_data} test="wptas"></Linechart>
+            <Linechart data={abs_data} test="abs"></Linechart>
+        </div>
     );
 }
