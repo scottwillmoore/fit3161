edd3912d3832abad5662b01b5b0fcfebea1a320a
Christopher Ryan Limoa <clim0061@student.monash.edu>
2021-05-10 21:21:56 +1000

    Added subscales of ABS results to chart

--- src/components/linechart.tsx
+++ src/components/linechart.tsx
@@ -1,20 +1,12 @@
-import { Children } from "react";
 import { Line } from "react-chartjs-2";
 
-// // static data example
-// const wptas_data = {
-//     labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
-//     datasets: [
-//         {
-//             label: "WPTAS",
-//             data: [5, 6, 9, 9, 10, 12, 12],
-//             fill: true,
-//             backgroundColor: "rgba(75,192,192,0.2)",
-//             borderColor: "rgba(75,192,192,1)",
-//             tension: 0.3,
-//         },
-//     ],
-// };
+const red_bg = "rgba(230, 41, 55, 0.3)";
+const green_bg = "rgba(120, 190, 172, 0.3)";
+const blue_bg = "rgba(54, 162, 235, 0.3)";
+
+const red_border = "rgba(230, 41, 55, 1)";
+const green_border = "rgba(120, 190, 172, 1)";
+const blue_border = "rgba(54, 162, 235, 1)";
 
 const options = {
     scales: {
@@ -25,37 +17,83 @@ const options = {
     },
 };
 
+let sum = (array: number[]): number => {
+    return array.reduce((sum, current) => sum + current, 0);
+};
+
 export type LinechartProps = {
-    data: Array<number>;
+    data: number[][];
     test: string;
 };
 
 export function Linechart({ data, test }: LinechartProps) {
-    // dynamic data example
-    let label, backgroundColor, borderColor;
+    let label, backgroundColor, borderColor, dataset, selected_data;
 
     if (test == "wptas") {
         label = "WPTAS";
         backgroundColor = "rgba(75,192,192,0.2)";
         borderColor = "rgba(75,192,192,1)";
-    } else if (test == "abs") {
-        label = "ABS";
-        backgroundColor = "rgba(192,104,75,0.2)";
-        borderColor = "rgba(192,104,75,1)";
-    }
-
-    const test_data = {
-        labels: [...Array(data.length).keys()].map((x) => "Day " + (x + 1)),
-        datasets: [
+        dataset = [
             {
                 label: label,
-                data: data,
+                data: data.map((x) => sum(x)),
                 fill: true,
                 backgroundColor: backgroundColor,
                 borderColor: borderColor,
                 tension: 0.2,
             },
-        ],
+        ];
+    } else if (test == "abs") {
+        backgroundColor = [green_bg, red_bg, blue_bg];
+        borderColor = [green_border, red_border, blue_border];
+        selected_data = [
+            data.map((x: number[]) => x.slice(0, 3).concat(x.slice(5, 10))),
+            data.map((x: number[]) => x.slice(2, 5).concat(x.slice(13, 14))),
+            data.map((x: number[]) => x.slice(10, 13)),
+        ];
+
+        dataset = [
+            {
+                label: "Disinhibition",
+                data: selected_data[0].map((x) => sum(x)),
+                fill: true,
+                backgroundColor: backgroundColor[0],
+                borderColor: borderColor[0],
+                tension: 0.2,
+            },
+            {
+                label: "Aggression",
+                data: selected_data[1].map((x) => sum(x)),
+                fill: true,
+                backgroundColor: backgroundColor[1],
+                borderColor: borderColor[1],
+                tension: 0.2,
+            },
+            {
+                label: "Lability",
+                data: selected_data[2].map((x) => sum(x)),
+                fill: true,
+                backgroundColor: backgroundColor[2],
+                borderColor: borderColor[2],
+                tension: 0.2,
+            },
+        ];
+    }
+
+    const days = [...Array(data.length).keys()].map((x) => "Day " + (x + 1));
+
+    const scores = data.map((x) => sum(x));
+
+    const labels = [];
+
+    for (let i = 0; i < days.length; i++) {
+        labels.push([days[i], "Score: " + scores[i]]);
+    }
+
+    // dynamic data
+    const test_data = {
+        labels: labels,
+        datasets: dataset,
     };
 
     return <Line data={test_data} options={options} type="line" />;
