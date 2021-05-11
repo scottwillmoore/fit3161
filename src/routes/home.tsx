5e3f1f80caa75c5444d619360b10c01a7bfadb67
Angus Dunn <anguslawlessdunn@gmail.com>
2021-05-11 10:25:46 +1000

    Merge branch 'master' of https://github.com/scottwillmoore/fit3161

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,8 +5,13 @@ import { Button, ButtonGroup, Input } from "@/components";
 import { Flame, Search } from "@/icons";
 import { generateId } from "@/models";
 
+import QRCode from "qrcode";
+import QrReader from "react-qr-reader";
+
 export function Home() {
     const history = useHistory();
+    const [imageUrl, setImageUrl] = useState("");
+    const [scanResultWebCam, setScanResultWebCam] = useState("");
 
     const [patientId, setPatientId] = useState("eEU9EsmPZiD4Y5VfDQgWLZ");
 
@@ -22,13 +27,50 @@ export function Home() {
         history.push(`/patient/${patientId}`);
     };
 
+    const generateQrCode = async () => {
+        try {
+            const response = await QRCode.toDataURL(patientId);
+            setImageUrl(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleErrorWebCam = (error) => {
+        console.log(error);
+    };
+
+    const handleScanWebCam = (result) => {
+        if (result) {
+            setScanResultWebCam(result);
+        }
+    };
+
     return (
         <Fragment>
             <Input value={patientId} onChange={handleChange} />
             <ButtonGroup>
                 <Button icon={Flame} text="Random" onClick={handleRandom} />
                 <Button icon={Search} text="Find" onClick={handleSearch} />
+                <Button
+                    icon={ScreenFull}
+                    text="Generate"
+                    onClick={generateQrCode}
+                />
             </ButtonGroup>
+
+            {imageUrl ? (
+                <img src={imageUrl} alt="img" width="200" height="200" />
+            ) : null}
+
+            <QrReader
+                delay={300}
+                style={{ width: "100%" }}
+                onError={handleErrorWebCam}
+                onScan={handleScanWebCam}
+            />
+
+            <Input value={scanResultWebCam} />
         </Fragment>
     );
 }
