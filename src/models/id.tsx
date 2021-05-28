86660939c3499ba8a4b67996e1cb03e7b1a754cf
Angus Dunn <anguslawlessdunn@gmail.com>
2021-05-28 22:37:55 +1000

    Added comments to the id page

--- src/models/id.tsx
+++ src/models/id.tsx
@@ -3,11 +3,20 @@ import { v4 as generateUuid, validate as validateUuid } from "uuid";
 
 const translator = newTranslator();
 
+/**
+ * Function to generate new a UUID
+ * @returns a new user unique identifier (short)
+ */
 export function generateId() {
     const uuid = generateUuid();
     return translator.fromUUID(uuid);
 }
 
+/**
+ * Function to test the validity of UUIDs
+ * @param id The unique user identification to be checked
+ * @returns returns true/false depending on if the id is valid
+ */
 export function validateId(id: string) {
     //return true;
     const uuid = translator.toUUID(id);
